f026312fcbe5f01a5c6bab440aa5a77bb7671557
Aaron-Hsiao1 <aaronh8227@gmail.com>
2024-11-19 00:27:36 -0800

    added explosion reworked movement

--- Assets/Scripts/BoomerrangMech.cs
+++ Assets/Scripts/BoomerrangMech.cs
@@ -21,6 +21,14 @@ public class BoomerrangMech : MonoBehaviour
 	private bool boomercreated;
 	public bool goBack;
 
+	[Header("Shockwave")]
+	[SerializeField] private float radius = 10f;
+	[SerializeField] private float force = 10f;
+	[SerializeField] private float upwardModifier = 1f;
+
+	[SerializeField] private Rigidbody rb;
+	[SerializeField] private Rigidbody testingRb;
+
 	bool readyToThrow;
 	// Start is called once before the first execution of Update after the MonoBehaviour is created
 	void Start()
@@ -45,22 +53,21 @@ public class BoomerrangMech : MonoBehaviour
 		{
 			projectile.transform.position = Vector3.MoveTowards(projectile.transform.position, player.transform.position, 0.5f);
 		}
+
+		if (Input.GetKeyDown(KeyCode.U))
+		{
+			TestForceADd();
+		}
 	}
 
 	private void Throw()
 	{
 		readyToThrow = false;
-
 		Destroy(holdingBoomer);
-
 		projectile = Instantiate(objectToThrow, attackPoint.position, cam.rotation);
-
 		//Debug.Log("ball created");
-
 		Rigidbody projectileRb = projectile.GetComponent<Rigidbody>();
-
 		Vector3 forceDirection = cam.transform.forward;
-
 		RaycastHit hit;
 
 		if (Physics.Raycast(cam.position, cam.forward, out hit, 500f))
@@ -69,9 +76,7 @@ public class BoomerrangMech : MonoBehaviour
 		}
 
 		Vector3 forceToAdd = forceDirection * throwForce + transform.up * throwUpwardForce;
-
 		projectileRb.AddForce(forceToAdd, ForceMode.Impulse);
-
 		//remove later this is just to test
 	}
 
@@ -99,4 +104,36 @@ public class BoomerrangMech : MonoBehaviour
 		holdingBoomer.transform.SetParent(holdSpawn.transform, true);
 		//Debug.Log("Spawning");
 	}
+
+	public void TriggerShockwave(Vector3 position)
+	{
+		Debug.Log("Shockwave triggered");
+
+		Collider[] colliders = Physics.OverlapSphere(position, radius);
+		foreach (Collider collider in colliders)
+		{
+			Rigidbody rb = collider.GetComponent<Rigidbody>();
+			if (rb != null && collider.gameObject.CompareTag("Player"))
+			{
+				Debug.Log("shockwave actually triggered");
+				Debug.Log("rb tag: " + collider.gameObject.name);
+				rb = collider.GetComponent<Rigidbody>();
+				rb.AddExplosionForce(force, position, radius, upwardModifier, ForceMode.Impulse);
+				break;
+			}
+		}
+	}
+
+	private void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.blue;
+		Gizmos.DrawWireSphere(transform.position, radius);
+	}
+
+	private void TestForceADd()
+	{
+		Debug.Log("forcr added");
+		testingRb.AddExplosionForce(force, testingRb.position, radius, upwardModifier, ForceMode.Impulse);
+	}
+
 }
