b25bdcf7bfda88e586b2d9363ca86dea8ebc6657
CatGirlASF <nnguyenbriann@gmail.com>
2024-11-22 22:26:07 -0800

    map functiuons

--- Assets/Scripts/PlatformButton.cs
+++ Assets/Scripts/PlatformButton.cs
@@ -3,12 +3,19 @@ using UnityEngine;
 public class PlatformButton : MonoBehaviour
 {
 	[SerializeField] private GameObject platform;
+	Animator m_Animator;
 
-	private void OnCollisionEnter(Collision other)
+    private void Start()
+    {
+        m_Animator = gameObject.GetComponent<Animator>();
+    }
+    private void OnCollisionEnter(Collision other)
 	{
 		if (other.gameObject.CompareTag("Boomerrang"))
 		{
 			platform.SetActive(true);
+
+			m_Animator.SetTrigger("Pressed");
 		}
 	}
 }
