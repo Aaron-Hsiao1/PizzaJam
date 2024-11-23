bac7c803f186a2d8b0fe1907b59382da5fa89d17
Aaron-Hsiao1 <aaronh8227@gmail.com>
2024-11-22 20:14:21 -0800

    brian

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	[Header("Player")]
+	[SerializeField] private GameObject player;
+	[SerializeField] private GameObject spawnPoint;
+
+
+	public void KillPlayer()
+	{
+		player.GetComponent<Rigidbody>().transform.position = spawnPoint.transform.position + new Vector3(0, 1, 0);
+		Debug.Log("You Died!");
+	}
+}
