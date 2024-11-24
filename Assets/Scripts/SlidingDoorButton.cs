69bc7176bc8da3f4a18d365e495e71c03533e0aa
CatGirlASF <nnguyenbriann@gmail.com>
2024-11-23 22:21:06 -0800

    things were added to tutorial

--- Assets/Scripts/SlidingDoorButton.cs
+++ Assets/Scripts/SlidingDoorButton.cs
@@ -3,13 +3,15 @@ using UnityEngine;
 public class SlidingDoorButton : MonoBehaviour
 {
 	[SerializeField] private SlidingDoor slidingDoor;
+    Animator m_Animator;
 
-	private void OnCollisionEnter(Collision other)
+    private void OnCollisionEnter(Collision other)
 	{
 		if (other.gameObject.CompareTag("Boomerrang") && slidingDoor.Moving == false)
 		{
 			Debug.Log("collision");
 			slidingDoor.Moving = true;
-		}
+            m_Animator.SetTrigger("Pressed");
+        }
 	}
 }
